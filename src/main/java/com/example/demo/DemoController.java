92590cc4feb774b1931bd1d9712423871a54b29e
Kevin Kammleiter <62kake1bif@hft-stuttgart.de>
2020-06-25 22:08:59 +0200

    init commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String getHtml(Model model) {
+        return "index";
+    }
+}
